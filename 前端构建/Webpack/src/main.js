81154e0dd1ed77253a593efb02a74a6302540a55
yanpengli <larry1314mvp@gmail.com>
2023-05-31 16:42:54 +0800

    优化webpack配置

--- 前端构建/Webpack/src/main.js
+++ 前端构建/Webpack/src/main.js
@@ -1,48 +1,64 @@
 import { getDate } from "@/utils";
 
-import '@/style/common.scss'
+import "@/style/common.scss";
 
-import content from './what.abc'
+import content from "./what.abc";
 
-import Vue from 'vue'
+import icon from "@/assets/images/icon.jpeg";
 
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
+// import Vue from 'vue'
 
-import App from '@/vue/app.vue'
-import router from '@/vue/router'
+// import ElementUI from 'element-ui'
+// import 'element-ui/lib/theme-chalk/index.css'
+
+// import App from '@/vue/app.vue'
+// import router from '@/vue/router'
 // 开发环境才需要use 生产环境引用CDN资源
 
-Vue.use(ElementUI)
+// Vue.use(ElementUI)
 // if (process.env.NODE_ENV !== 'production') {
 //     // 如果用了CDN的资源 这里就不能再使用use了 因为ElementUI和暴露再window上的对象名称不一样 会报ElementUI未定义的错误
 //     Vue.use(ElementUI)
 // }
 
+console.log(process.env);
 function printDate() {
-    console.log(getDate() + 10)
+  console.log(getDate() + 1110);
 }
 
 function bootstrap() {
-    printDate()
+  printDate();
 
-    document.onreadystatechange = (ev) => {
-        console.log(ev)
-    }
+  document.onreadystatechange = (ev) => {
+    console.log(ev);
+  };
 
-    window.onload = (e) => {
-        document.getElementById('title').addEventListener('click', (e) => {
-            e.target.innerText = `通过一个自定义的loader获取一个特殊文件里面的内容${content}`
-        })
-    }
+  window.onload = (e) => {
+    document.getElementById("title").addEventListener("click", (e) => {
+      e.target.innerText = `通过一个自定义的loader获取一个特殊文件里面的内容${content}`;
+    });
 
-    new Vue({
-        router,
-        render(h) {
-            return h(App)
-        },
-        el: '#vueApp'
-    })
+    let img = document.createElement("img");
+    img.src = icon;
+
+    document.querySelector('.container').appendChild(img);
+
+    let image = new Image(100, 100);
+    image.src = icon;
+
+    document.querySelector('.container').appendChild(image);
+    image.onload = function (e) {
+      console.log("----------");
+    };
+  };
+
+  // new Vue({
+  //     router,
+  //     render(h) {
+  //         return h(App)
+  //     },
+  //     el: '#vueApp'
+  // })
 }
 
-bootstrap()
+bootstrap();
--- 前端构建/Webpack/webpack.config.js
+++ 前端构建/Webpack/webpack.config.js
@@ -1,26 +1,26 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-// const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const CDNWebpackPlugin = require('./plugins/cdn-webpack-plugin')
-const HooksWebpackPlugin = require('./plugins/hooks-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const CompressionWebpackPlugin = require('compression-webpack-plugin')
-const { VueLoaderPlugin } = require('vue-loader')
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
-const DefinePlugin = require('webpack/lib/DefinePlugin')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin"); // html模版插件 用于将构建内容直接插入html中
+// const { CleanWebpackPlugin } = require('clean-webpack-plugin') // 清空构建目录
+const CDNWebpackPlugin = require("./plugins/cdn-webpack-plugin"); // 自定义插件一 往html页面中插入一些资源，如css、js等 HtmlWebpackPlugin同样能完成该功能
+const HooksWebpackPlugin = require("./plugins/hooks-webpack-plugin"); // 自定义插件二 查看各个hooks的触发时机
+const MiniCssExtractPlugin = require("mini-css-extract-plugin"); // 导出css插件 把css代码从js中分离出来
+const CompressionWebpackPlugin = require("compression-webpack-plugin"); // 代码压缩插件
+const { VueLoaderPlugin } = require("vue-loader"); // 识别.vue文件的loader
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer"); // 分析打包的结构，通过分析构建的结果，去优化webpack构建过程
+const DefinePlugin = require("webpack/lib/DefinePlugin"); // 定义全局变量，可以在代码中直接使用
+const BannerPlugin = require("webpack/lib/BannerPlugin"); // 添加类似版权信息的插件
 
-const isDev = process.env.NODE_ENV !== "production"
+const isDev = process.env.NODE_ENV !== "production";
 
 function resolve(name) {
-    return path.resolve(__dirname, name)
+  return path.resolve(__dirname, name);
 }
 
 let cdns = {
-    css: [],
-    js: []
-}
-let externals = {
-}
+  css: [],
+  js: [],
+};
+let externals = {};
 
 // 采用这种方式可以加载CDN资源
 // if (!isDev) {
@@ -42,155 +42,185 @@ let externals = {
 // }
 
 let plugins = [
-    // new CleanWebpackPlugin(), // 清空构建目录
-    new HtmlWebpackPlugin({ // 自动将打包后的代码注入到html页面中
-        template: resolve('template/index.html'), // 使用index-cdn.html
-        title: 'Webpack学习',
-        // 生产环境使用CDN资源 原理就是在html页面中拿到这些参数 使用模板渲染引擎去把这些资源渲染出来
-        // cdnConfig: cdns 
-    }),
-    // 自己写CDN的插件 
-    new CDNWebpackPlugin({ // 自定义测试插件 把下面的CDN内容添加到html页面中
-        cdns
-    }),
-    new HooksWebpackPlugin(), // 自定义测试插件 测试常见的hook
-    new VueLoaderPlugin(), // 支持vue
+//   new CleanWebpackPlugin(), // 清空构建目录 v5版本不再需要此插件 直接在output中配置clean即可
+  new HtmlWebpackPlugin({
+    // 自动将打包后的代码注入到html页面中
+    template: resolve("template/index.html"), // 使用index-cdn.html 原始模版
+    title: "Webpack学习", // 传递给页面的title，可以在模版中引用
+    filename: 'index.html', // 输出的html名称
+    // 生产环境使用CDN资源 原理就是在html页面中拿到这些参数 使用模板渲染引擎去把这些资源渲染出来
+    // cdnConfig: cdns  //这里的参数都可以传递到模版中，支持模版语法获取所有的参数并使用
+  }),
+  // 自己写CDN的插件
+  new CDNWebpackPlugin({
+    // 自定义测试插件 把下面的CDN内容添加到html页面中
+    cdns,
+  }),
+  new HooksWebpackPlugin(), // 自定义测试插件 测试常见的hook
+  new VueLoaderPlugin(), // 支持vue
 
-    // new BundleAnalyzerPlugin(), // 打包分析插件
+  // new BundleAnalyzerPlugin(), // 打包分析插件
 
-    // 定义常量 可以在编码时直接使用这里定义的常量 webpack编译时，会把代码中遇到的该常量直接替换成具体得值
-    new DefinePlugin({
-        'process.env': {
-            NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development') // 把打包的环境信息注入到代码内
-        },
-        'PI': {
-            value: Math.PI
-        }
-    })
-]
+  // 定义常量 可以在编码时直接使用这里定义的常量 webpack编译时，会把代码中遇到的该常量直接替换成具体得值
+  new DefinePlugin({
+    'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+        DEBUG: JSON.stringify(process.env.DEBUG)
+    },
+    PI: Math.PI,
+  }),
+  new BannerPlugin({
+    banner: "版权所有，翻版必究 fullhash:[fullhash], chunkhash:[chunkhash], name:[name], base:[base], query:[query], file:[file]",
+  }),
+];
 
 if (!isDev) {
-    plugins.push(new MiniCssExtractPlugin({
-        filename: '[name].[chunkhash:8].css'
-    }))// 生产环境时，提取css到单独的文件
+  plugins.push(
+    new MiniCssExtractPlugin({
+      filename: "css/[name].[chunkhash:8].css",
+    })
+  ); // 生产环境时，提取css到单独的文件
 
-    // 生成gzip文件资源
-    // plugins.push(new CompressionWebpackPlugin({
-    //     test: /\.js(\?.*)?$/i,
-    //     algorithm: 'gzip',
-    //     compressionOptions: { level: 1 },
-    //     threshold: 0,
-    //     minRatio: 0.8,
-    //     deleteOriginalAssets: false,
-    // }))
+  // 生成gzip文件资源
+  // plugins.push(new CompressionWebpackPlugin({
+  //     test: /\.js(\?.*)?$/i,
+  //     algorithm: 'gzip',
+  //     compressionOptions: { level: 1 },
+  //     threshold: 0,
+  //     minRatio: 0.8,
+  //     deleteOriginalAssets: false,
+  // }))
 }
 
 module.exports = {
-    mode: isDev ? 'development' : 'production', // production development
-    // target: 'async-node',
-    // 入口文件配置
-    devServer: {
-        hot: true, // 开启热更新
-        static: resolve('dist'), // 将dist目录下的文件 作为额外可访问文件
-        port: 8090, // 启动服务的端口
-        compress: true, // 启动gzip压缩
-        open: true, // 自动打开
+  mode: isDev ? "development" : "production", // production development
+  // target: 'async-node',
+  // 入口文件配置
+  devServer: {
+    hot: true, // 开启热更新
+    static: resolve("dist"), // 将dist目录下的文件 作为额外可访问文件
+    port: 8090, // 启动服务的端口
+    compress: true, // 启动gzip压缩
+    open: true, // 自动打开
+  },
+  // devtool: 'source-map',
+  context: resolve("src"),
+  entry: {
+    main: "./main.js",
+    // common: ['./utils/index.js']
+  },
+  resolve: {
+    alias: {
+      "@": resolve("src"),
     },
-    // devtool: 'source-map',
-    context: resolve('src'),
-    entry: {
-        main: './main.js',
-        // common: ['./utils/index.js']
+  },
+  // 85dc0fc38a6b9c0f99e8
+  output: {
+    // 40032daaeb439fb070bc-eaebe637f6cc33143579
+    /**
+     * hash、chunkhash、contenthash的区别, 不同的hash值可以利用缓存技术提升构建速度
+     * 1. 通过配置多个entry入口可以知道 hash计算是跟整个项目的构建相关，同一次构建过程中生成的哈希都是一样的
+     * 2. 由于采用chunkhash，所以项目主入口文件main.js及其对应的依赖文件common.css由于被打包在同一个模块，所以共用相同的chunkhash，但是公共库由于是不同的模块，所以有单独的chunkhash。这样子就保证了在线上构建的时候只要文件内容没有更改就不会重复构建
+     * 3. contenthash这是相比第二点来说，main.js和common.css有不同的hash值，main.js有改动 但是common.css没改动 common.css就不会重新打包
+     */
+    clean: true, // 有了这个就不用再使用CleanWebpackPlugin
+    filename: "[name].[chunkhash:8].js",
+    path: resolve("dist"),
+  },
+  optimization: {
+    nodeEnv: false,
+    minimize: false,
+    usedExports: true, // Tree shaking 只打哪些使用的代码 减少代码打包的体积
+    splitChunks: {
+      // 可选值有async、initial、all
+      // chunks: 'all',
+      minSize: 0,
+      // 表示拆分出的chunk的名称连接符。默认为~。如chunk~vendors.js
+      // automaticNameDelimiter: '~',
+      // 设置chunk的文件名。默认为true。当为true时，splitChunks基于chunk和cacheGroups的key自动命名。
+      // name: 'chunk-[chunkhash:8].js',
+      // cacheGroups: {
+      //     vendors: {
+      //         test: /[\\/]node_modules[\\/]/,
+      //         priority: -10
+      //     },
+      // }
     },
-    resolve: {
-        alias: {
-            '@': resolve('src')
-        },
-    },
-    entry: './main.js',
-    // 85dc0fc38a6b9c0f99e8
-    output: {
-        // 40032daaeb439fb070bc-eaebe637f6cc33143579
-        /**
-         * hash、chunkhash、contenthash的区别, 不同的hash值可以利用缓存技术提升构建速度
-         * 1. 通过配置多个entry入口可以知道 hash计算是跟整个项目的构建相关，同一次构建过程中生成的哈希都是一样的
-         * 2. 由于采用chunkhash，所以项目主入口文件main.js及其对应的依赖文件common.css由于被打包在同一个模块，所以共用相同的chunkhash，但是公共库由于是不同的模块，所以有单独的chunkhash。这样子就保证了在线上构建的时候只要文件内容没有更改就不会重复构建
-         * 3. contenthash这是相比第二点来说，main.js和common.css有不同的hash值，main.js有改动 但是common.css没改动 common.css就不会重新打包
-         */
-        clean: true,
-        filename: '[name].[chunkhash:8].js',
-        path: resolve('dist'),
-    },
-    optimization: {
-        minimize: false,
-        usedExports: true,  // Tree shaking 只打哪些使用的代码 减少代码打包的体积
-        splitChunks: {
-            // 可选值有async、initial、all
-            // chunks: 'all',
-            minSize: 0,
-            // 表示拆分出的chunk的名称连接符。默认为~。如chunk~vendors.js
-            // automaticNameDelimiter: '~',
-            // 设置chunk的文件名。默认为true。当为true时，splitChunks基于chunk和cacheGroups的key自动命名。
-            // name: 'chunk-[chunkhash:8].js',
-            // cacheGroups: {
-            //     vendors: {
-            //         test: /[\\/]node_modules[\\/]/,
-            //         priority: -10
-            //     },
-            // }
-        },
-    },
+  },
 
-    externals,
-    resolveLoader: {
-        alias: { // 给loader取一个别名
-            'abc-loader': 'a-loader',
-            'replace-loader': 'b-loader'
-        },
-        modules: ['node_modules', 'loaders'], // 配置loader的查找路径
+  externals,
+  resolveLoader: {
+    alias: {
+      // 给loader取一个别名
+      "abc-loader": "a-loader",
+      "replace-loader": "b-loader",
     },
-    module: {
-        // 对不同文件处理的loader 
-        rules: [{
-            test: /\.vue$/,
-            use: [{
-                loader: 'vue-loader'
-            }]
-        }, {
-            test: /\.(sa|sc|c)ss$/, // 匹配文件类型
-            // 要使用的loader use数组中 loader执行的顺序是从后往前
-            use: [{
-                loader: isDev ? 'style-loader' : MiniCssExtractPlugin.loader,  // 把CSS插入到DOM中
-                options: isDev ? {} : {
-                    esModule: false,
+    modules: ["node_modules", "loaders"], // 配置loader的查找路径
+  },
+  module: {
+    // 对不同文件处理的loader
+    rules: [
+      {
+        // 处理vue文件
+        test: /\.vue$/,
+        use: [
+          {
+            loader: "vue-loader",
+          },
+        ],
+      },
+      {
+        // 处理css相关文件
+        test: /\.(sa|sc|c)ss$/, // 匹配文件类型
+        // 要使用的loader use数组中 loader执行的顺序是从后往前
+        use: [
+          {
+            loader: isDev ? "style-loader" : MiniCssExtractPlugin.loader, // 把CSS插入到DOM中
+            options: isDev
+              ? {}
+              : {
+                  esModule: false,
                 },
-            }, {
-                loader: 'css-loader', // 识别css模板
-                options: {
-                    modules: false,
-                    // url: true 
-                }
-            }, {
-                loader: 'sass-loader',
-            }, {
-                loader: 'postcss-loader', // css兼容处理
-                options: {
-                    postcssOptions: {
-                        plugins: [['postcss-preset-env', {
-
-                        }]]
-                    }
-                }
-            }]
-        }, {
-            test: /\.abc$/,
-            use: ['abc-loader', {
-                loader: 'replace-loader',
-                options: {
-                    word: "%%%%%"
-                }
-            }]
-        }]
-    },
-    plugins
-}
+          },
+          {
+            loader: "css-loader", // 识别css模板
+            options: {
+              modules: false,
+              // url: true
+            },
+          },
+          {
+            loader: "postcss-loader", // css兼容处理 参数在postcss.config.js中
+          },
+          {
+            loader: "sass-loader",
+          },
+        ],
+      },
+      {
+        // 自定义测试loader 处理以.abc结尾的文件
+        test: /\.abc$/,
+        use: [
+          "abc-loader",
+          {
+            loader: "replace-loader",
+            options: {
+              word: "%%%%%",
+            },
+          },
+        ],
+      },
+      {
+        // 处理图片等资源文件
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource'
+      },
+      {
+        // 字体和svg
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: 'asset/inline',
+      },
+    ],
+  },
+  plugins,
+};
